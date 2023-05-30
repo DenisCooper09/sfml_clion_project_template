336460c27910587d8d9fafe033976dc128a81d49
Denis Cooper <denis.cooper09@gmail.com>
2023-05-30 13:05:30 +0100

    Link SFML: create window

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+
+    const uint32_t WINDOW_WIDTH = 1200;
+    const uint32_t WINDOW_HEIGHT = 800;
+
+    sf::RenderWindow window({WINDOW_WIDTH, WINDOW_HEIGHT},
+                            "SFML Window",
+                            sf::Style::Close,
+                            settings);
+
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::EventType::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::White);
+        window.display();
+    }
+
     return EXIT_SUCCESS;
 }
